8579348bc1aa4a5c15d52e0f5c82dddb27f6103e
MarcSchmelter <s8mascme@stud.uni-saarland.de>
2023-05-26 22:13:25 +0200

    some functions, still adding more

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,25 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_PN532.h>
+#include <Servo.h>
 
 Adafruit_PN532 nfc(-1, -1);
 void checkNFC();
+Servo setServo, dispenseServo;
+int setting[] = {100, 200, 300, 500, 700, 1000, 0};
+int currentSetting = 2;
+
+bool checkDistance(void);
+
 
 void setup() {
   Serial.begin(115200);
+  pinMode(D5, OUTPUT);
+  pinMode(D6, INPUT);
+  setServo.attach(D3);
+  dispenseServo.attach(D4);
+  setServo.write(90);
+  dispenseServo.write(90);
 
   nfc.begin();
   if (! nfc.getFirmwareVersion()) {
@@ -18,7 +31,7 @@ void setup() {
 
 void loop() {
   checkNFC();
-  delay(100);
+  delay(2000);
 }
 
 
@@ -38,6 +51,45 @@ void checkNFC()
     Serial.println(" bytes");
     Serial.print("  UID Value: ");
     nfc.PrintHex(uid, uidLength);
-    Serial.println("");
+    for (int i = 0; i < uidLength; i++)
+    {
+      Serial.println(uid[i]);
+    }
+    uint8_t data[4];
+    nfc.mifareultralight_ReadPage(4, data);
+
+    
+
+    for (int i = 0; i < 4; i++)
+    {
+      Serial.println(data[i]);
+    }
+    if (data[0] == 0)   
+    {
+      data[1] += amounts[setting];
+      nfc.mifareultralight_WritePage(4, data);
+    }
+
+    
+
+bool checkDistance(void){
+  digitalWrite(D5, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(D5, LOW);
+  long duration = pulseIn(D6, HIGH);
+  float distance = duration * 0.034 / 2;
+  Serial.println("The distance is " + String(distance) + " cm.");
+  //return distance <= 2.5;
+  return distance <= 4.0;
+}
+
+    
+    
+    
+
+    
+    
+
+    
   }
 }
